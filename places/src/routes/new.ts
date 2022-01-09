46dcaf6c9a559480aa38f62f31cb13b364e5b702
vitoraa <vitor.lima@outlook.com>
2022-01-09 11:42:08 -0300

    should return an error if an invalid place is provided

--- places/src/routes/new.ts
+++ places/src/routes/new.ts
@@ -1,10 +1,24 @@
-import { requireAuth } from '@vitoraafastpayfood/common';
+import { requireAuth, validateRequest } from '@vitoraafastpayfood/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 import { Place } from '../models/places';
 
 const router = express.Router();
 
-router.post('/api/places', requireAuth, async (req: Request, res: Response) => {
+router.post('/api/places', requireAuth, [
+  body('name')
+    .not()
+    .isEmpty()
+    .withMessage('Name is required'),
+  body('address')
+    .not()
+    .isEmpty()
+    .withMessage('Address is required'),
+  body('type')
+    .not()
+    .isEmpty()
+    .withMessage('Type is required'),
+], validateRequest, async (req: Request, res: Response) => {
   res.status(200).send();
 });
 
