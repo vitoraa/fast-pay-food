fd5e9ce9fa0319ed67dd3aabcf004881b46c4013
vitoraa <vitor.lima@outlook.com>
2022-01-09 11:21:38 -0300

    test: should return all places

--- places/src/routes/__test__/index.test.ts
+++ places/src/routes/__test__/index.test.ts
@@ -1,5 +1,15 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Place } from '../../models/places';
+import mongoose from 'mongoose';
+
+const createPlace = () => {
+  return Place.build({
+    name: 'Place 1',
+    address: 'Address 1',
+    type: 'restaurant',
+  })
+};
 
 test('should have a route handler listening to api/places for get request', async () => {
   const response = await request(app)
@@ -24,4 +34,24 @@ test('should returns status other than 401 if the user is signed in', async () =
     .send({})
 
   expect(response.status).not.toEqual(401);
+});
+
+test('should return all places', async () => {
+  const place = createPlace();
+  await place.save();
+  const place2 = createPlace();
+  await place2.save();
+  const place3 = createPlace();
+  await place3.save();
+
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .get('/api/places')
+    .set('Cookie', cookie)
+    .send({})
+
+  console.log(response.body);
+  expect(response.status).toEqual(200);
+  expect(response.body.length).toEqual(3);
 });
