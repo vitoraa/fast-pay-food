9db3bb3c4d9a9ebb7cafd215526de9afe205d146
vitoraa <vitor.lima@outlook.com>
2022-01-09 10:59:17 -0300

    should only be accessed if the user is signed in

--- places/src/routes/index.ts
+++ places/src/routes/index.ts
@@ -1,8 +1,9 @@
+import { requireAuth } from '@vitoraafastpayfood/common';
 import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.get('/api/places', async (req: Request, res: Response) => {
+router.get('/api/places', requireAuth, async (req: Request, res: Response) => {
   res.status(200).send();
 });
 
