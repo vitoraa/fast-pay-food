a34395dde612cf473ddd90f6edc7aade4e50d8da
vitoraa <vitor.lima@outlook.com>
2022-01-09 11:21:44 -0300

    should return all places

--- places/src/routes/index.ts
+++ places/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { requireAuth } from '@vitoraafastpayfood/common';
 import express, { Request, Response } from 'express';
+import { Place } from '../models/places';
 
 const router = express.Router();
 
 router.get('/api/places', requireAuth, async (req: Request, res: Response) => {
-  res.status(200).send();
+  const places = await Place.find({});
+  res.status(200).send(places);
 });
 
 export { router as indexPlaceRouter };
