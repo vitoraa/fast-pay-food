25fe05a0b8194c1d71579bcef4e856355e9015f0
vitoraa <vitor.lima@outlook.com>
2022-01-12 09:24:22 -0300

    should have a route handler listening to /api/places/:idPlace/food-category for post request

---
+++ packages/places/src/routes/food-category/__test__/new.test.ts
@@ -0,0 +1,21 @@
+import request from 'supertest';
+import { app } from '../../../app';
+import { Place } from '../../../models/place';
+
+const createPlace = () => {
+  return Place.build({
+    name: 'Place 1',
+    address: 'Address 1',
+    type: 'restaurant',
+  })
+};
+
+test('should have a route handler listening to /api/places/:idPlace/food-category for post request', async () => {
+  const place = createPlace();
+  await place.save();
+  const response = await request(app)
+    .post(`/api/places/${place.id}/food-category`)
+    .send({ name: 'name' })
+
+  expect(response.status).not.toEqual(404);
+});
